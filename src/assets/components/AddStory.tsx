9dcbd0676ed9c1fea934200bfa3ee273a11f9238
developerbrm <developerbrm@gmail.com>
2025-03-12 11:26:00 +0530

    - change listener added

--- src/assets/components/AddStory.tsx
+++ src/assets/components/AddStory.tsx
@@ -1,15 +1,28 @@
+import React from 'react'
 import { IoAddOutline } from 'react-icons/io5'
 
 const AddStory = () => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const filesUrls = [...e.target.files].map(URL.createObjectURL)
+
+    console.log(filesUrls)
+  }
+
   return (
     <div>
-      <button
-        name="add-stories"
-        title="add-stories"
-        className="grid aspect-square w-14 cursor-pointer place-content-center rounded-full text-slate-600 ring-2 ring-slate-600"
-      >
+      <label className="grid aspect-square w-14 cursor-pointer place-content-center rounded-full text-slate-600 ring-2 ring-slate-600">
         <IoAddOutline size={22} strokeWidth={2} />
-      </button>
+
+        <input
+          multiple
+          className="hidden"
+          name="add-stories"
+          title="add-stories"
+          type="file"
+          accept="video/*, image/*"
+          onChange={handleChange}
+        />
+      </label>
     </div>
   )
 }
